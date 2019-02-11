b078d34587e5aa23729dc375873ebbf2cec20d47
Prince295 <ivankochegarov1@gmail.com>
2019-02-11 20:38:30 +0300

    0.2

--- main.py
+++ main.py
@@ -19,17 +19,86 @@ class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
             for file_name in os.listdir(directory):
                 self.listWidget.addItem(file_name)
 
+
 class CCalculator(QtWidgets.QWidget, Ui_Form):
     def __init__(self, parent):
         super().__init__()
         self.result =''
         self.setupUi(self)
         self.oneButton.clicked.connect(self.setOneButton)
-
+        self.twoButton.clicked.connect(self.setTwoButton)
+        self.threeButton.clicked.connect(self.setThreeButton)
+        self.fourButton.clicked.connect(self.setFourButton)
+        self.fiveButton.clicked.connect(self.setFiveButton)
+        self.sixButton.clicked.connect(self.setSixButton)
+        self.sevenButton.clicked.connect(self.setSevenButton)
+        self.eightButton.clicked.connect(self.setEightButton)
+        self.nineButton.clicked.connect(self.setNineButton)
+        self.zeroButton.clicked.connect(self.setZeroButton)
+        self.zzButton.clicked.connect(self.setZZButton)
+        self.plusButton.clicked.connect(self.setPlusButton)
+        self.minusButton.clicked.connect(self.setMinusButton)
+        self.umnButton.clicked.connect(self.setUmnButton)
+        self.delenButton.clicked.connect(self.setDelenButton)
+        self.deleteButton.clicked.connect(self.setDeleteButton)
+        self.procentButton.clicked.connect(self.setProcentButton)
+        self.ravnoButton.clicked.connect(self.setRavnoButton)
+        self.clearButton.clicked.connect(self.setClearButton)
     def setOneButton(self):
         self.result += '1'
         self.lineEdit.setText(self.result)
+    def setTwoButton(self):
+        self.result += '2'
+        self.lineEdit.setText(self.result)
+    def setThreeButton(self):
+        self.result += '3'
+        self.lineEdit.setText(self.result)
+    def setFourButton(self):
+        self.result += '4'
+        self.lineEdit.setText(self.result)
+    def setFiveButton(self):
+        self.result += '5'
+        self.lineEdit.setText(self.result)
+    def setSixButton(self):
+        self.result += '6'
+        self.lineEdit.setText(self.result)
+    def setSevenButton(self):
+        self.result += '7'
+        self.lineEdit.setText(self.result)
+    def setEightButton(self):
+        self.result += '8'
+        self.lineEdit.setText(self.result)
+    def setNineButton(self):
+        self.result += '9'
+        self.lineEdit.setText(self.result)
+    def setZeroButton(self):
+        self.result += '0'
+        self.lineEdit.setText(self.result)
+    def setZZButton(self):
+        self.result += '00'
+        self.lineEdit.setText(self.result)
+    def setClearButton(self):
+        self.lineEdit.clear(self)
 
+    def setPlusButton(self):
+        pass
+
+    def setMinusButton(self):
+        pass
+
+    def setUmnButton(self):
+        pass
+    def setDelenButton(self):
+        pass
+
+    def setDeleteButton(self):
+        pass
+
+    def setProcentButton(self):
+        pass
+
+    def setRavnoButton(self):
+        pass
 
 
 sys._excepthook = sys.excepthook
